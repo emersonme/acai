e1fc674f4ee48b9bc71c74c41e1f27d46893f527
Emerson Mendes <emersonmesouza@gmail.com>
2020-01-11 20:56:34 -0400

    fix  authLayout

--- src/pages/_layouts/_auth/styles.js
+++ src/pages/_layouts/_auth/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import colors from '~/utils/colors'
 import {darken} from 'polished'
+import colors from '~/utils/colors'
 
 export const Wrapper = styled.div`
   height: 100%;
